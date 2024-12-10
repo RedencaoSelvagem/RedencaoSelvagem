fc4977ba30db6125d800316dd824f13c744db65f
ThomasStoianov <thomasgstoianov2@gmail.com>
2024-12-10 15:06:33 -0300

    subindo UIManager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Slider playerHealthBar;
+    public Image playerImage;
+
+    public GameObject enemyUI;
+    public Slider enemyHealthBar;
+    public Image enemyImage;
+
+    // Objeto para armazenar os dados do Player
+    private PlayerController player;
+
+    // Timers e Controllers do objeto enemyUI
+    [SerializeField] public float enemyUITime = 4f;
+    private float enemyTimer;
+    void Start()
+    {
+        // Obtem os dados do Player
+        player = FindFirstObjectByType<PlayerController>();
+
+        // Definir o valor máximo da barra de vida igual ao máximo da vida do Player
+        playerHealthBar.maxValue = player.maxHealth;
+
+        // Iniciar HealthBar cheia
+        playerHealthBar.value = playerHealthBar.maxValue;
+
+        // Definir a imagem do Player
+        playerImage.sprite = player.playerImage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Inicia o contador para controlar o tempo de exibição da enemyUI
+        enemyTimer += Time.deltaTime;
+
+        // Se o tempo limite for atingido, oculta a UI e reseta o Timer
+        if (enemyTimer >= enemyUITime)
+        {
+            enemyUI.SetActive(false);
+            enemyTimer = 0;
+        }
+    }
+
+    public void UpdatePlayerHealth(int amount)
+    {
+        playerHealthBar.value = amount;
+    }
+
+    public void UpdateEnemyUI(int maxHealth, int currentHealth, Sprite image)
+    {
+        // Atualiza os dados do inimigo de acordo com o inimigo atacado
+        enemyHealthBar.maxValue = maxHealth;
+        enemyHealthBar.value = currentHealth;
+        enemyImage.sprite = image;
+
+        // Zera o timer para começar a contar 4 segundos
+        enemyTimer = 0;
+
+        // Habilita a enemyUI, deixando a visível
+        enemyUI.SetActive(true);
+    }
+
+    // Método para exibir a enemyUI por apenas 4 segundos 
+}
